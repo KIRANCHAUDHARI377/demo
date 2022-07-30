abf4582760f382d165727ef01ca930423f0a624d
kiran <kiranchaudhari120@gmail.com>
2022-07-30 18:11:16 +0530

    first commit

---
+++ Day2_1.java
@@ -0,0 +1,37 @@
+
+public class Day2_1 {
+
+	public static void main(String[] args) {
+		int i=0;
+		System.out.println(i);
+           System.out.println("thank");
+            System.out.println("you");
+
+
+		float amount =23.6f;
+		double totalamount=45.45;
+		
+		
+		
+		//amount=(float)totalamount;
+		//System.out.println(amount);   //typecasting
+		
+		System.out.println(amount);
+		System.out.println(totalamount);
+		
+		boolean ispresent =false;
+		System.out.println(ispresent);
+		
+		char ch ='a';
+		System.out.println(ch);
+		System.out.println((int)ch);
+		
+		long l =12345;
+		//l=i;
+		i=(int)l;
+		System.out.println(i);  //typecasting
+		
+
+	}
+
+}
---
+++ Day2_2.java
@@ -0,0 +1,34 @@
+
+public class Day2_2 {
+
+	public static void main(String[] args) {
+		int i=0;
+		System.out.println(i);
+		
+		float amount =23.6f;
+		double totalamount=45.45;
+		
+		
+		System.out.println("lets start");
+		//amount=(float)totalamount;
+		//System.out.println(amount);   //typecasting
+		
+		System.out.println(amount);
+		System.out.println(totalamount);
+		
+		boolean ispresent =false;
+		System.out.println(ispresent);
+		
+		char ch ='a';
+		System.out.println(ch);
+		System.out.println((int)ch);
+		
+		long l =12345;
+		//l=i;
+		i=(int)l;
+		System.out.println(i);  //typecasting
+		
+
+	}
+
+}
